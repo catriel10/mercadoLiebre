06ec4e48ca948e4a1ca096de36dfdf950e04251b
catriel10 <catriel10@gmail.com>
2021-05-18 22:51:46 -0300

    prueba 3

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const path = require ("path");
 const publicPatch = path.resolve (__dirname,"./public")
 app.use( express.static(publicPatch));
 
-app.listen (process.env.PORT || 3030, ()=>{
+const port = process.env.PORT || 3030;
+
+app.listen (port, ()=>{
 console.log ("Mi servidor express esta funcionando en port 3030")
 });
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile (path.resolve (__dirname, "./views/home.html"))
 });
 

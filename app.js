c3badcf6ac46b2e88300dc62e1c9e3e688e6c65f
catriel10 <catriel10@gmail.com>
2021-05-18 22:07:00 -0300

    Arranco Heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require ("path");
 const publicPatch = path.resolve (__dirname,"./public")
 app.use( express.static(publicPatch));
 
-app.listen (3030, ()=>{
+app.listen (process.env.PORT || 3030, ()=>{
 console.log ("Mi servidor express esta funcionando en port 3030")
 });
 
